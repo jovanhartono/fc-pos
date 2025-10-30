eb41605920f93a8593d174ee6b817d69168982d6
jovan <jovanus@yahoo.co.id>
2025-10-30 16:06:34 +0700

    chore: remove unused file

--- packages/server/src/utils/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { zodValidator } from "@/utils/zod-validator-wrapper";
-
-export const idParamSchema = zodValidator(
-  "param",
-  z.object({
-    id: z.coerce.number({ message: "invalid number" }).int().positive(),
-  })
-);
